ac9ca96e45d2526b1eb198b021bc3b07021da3c2
Ansanat <Ansanat228@gmail.com>
2024-10-17 00:09:02 +0700

    init

---
+++ src/components/nav-bar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="nav-bar">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'nav-bar',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+.nav-bar {
+    width: 100%;
+    height: 50px;
+    background-color: red;
+}
+</style>
