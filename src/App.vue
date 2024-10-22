47935ef143e4857a6e27735fd449755804925f7e
vitaliy.koltochihin <vitaliy.koltochihin@eltex.loc>
2024-10-22 09:46:45 +0700

    add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <Navbar id="head"/>
   <router-view />
+  <Footer/>
 </template>
 
 <script>
 import Navbar from './components/nav-bar.vue'
+import Footer from './components/v-footer.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 }
 </script>
@@ -22,6 +25,5 @@ export default {
   color: #2c3e50;
   background-color: #ebebeb;
   border-radius: 10px;
-  padding-bottom: 500px;
 }
 </style>
---
+++ src/components/v-footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="v-footer">
+    <div class="v-footer-container">
+      <a href="https://t.me/rso_fake_news"><img src="@/img/telegram_logo.png" class="v-footer-icon"/></a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-footer',
+  props: {
+  }
+}
+</script>
+
+<style lang="scss">
+.v-footer {
+  width: 100%;
+  height: 60px;
+  background-color: #184678;
+  border-radius: 10px;
+  margin-top: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &-container {
+
+  }
+
+  &-icon {
+    width: 40px;
+  }
+}
+</style>
